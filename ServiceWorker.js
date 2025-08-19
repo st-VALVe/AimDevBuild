19d08f136a41315d94a9cf39b31f1e3ff22d1fcc
ValentinKirillov <st-valve@mail.ru>
2025-08-19 14:25:22 +0300

    test screen 0.0.037

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "stVALVe-Aim-0.0.25";
+const cacheName = "stVALVe-Aim-0.0.24";
 const contentToCache = [
-    "Build/AimDevBuild.loader.js",
-    "Build/AimDevBuild.framework.js",
-    "Build/AimDevBuild.data",
-    "Build/AimDevBuild.wasm",
+    "Build/92d2989954f394ef0758ef5acff5309e.loader.js",
+    "Build/eda317947ebfec35b99dca41ca417ee6.framework.js.unityweb",
+    "Build/fa6e4f88afe8133ceb76e170b0b2526a.data.unityweb",
+    "Build/e44735aa3465124e17e6db4674cf7f92.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
