06551834227855496c30ec2e2e90e42f620ca4ae
ValentinKirillov <st-valve@mail.ru>
2025-08-25 14:05:51 +0300

    Change version to 0.0.32

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,8 +1,8 @@
-const cacheName = "stVALVe-Aim-0.0.31";
+const cacheName = "stVALVe-Aim-0.0.32";
 const contentToCache = [
     "Build/8aed6607de7c7b2be89dd3679c6a36e6.loader.js",
     "Build/e65ebcf3b174cd8d6c2bd219d0ff30c5.framework.js.unityweb",
-    "Build/d49e3c42f1c54f53c10cf50b1d38a243.data.unityweb",
+    "Build/5b7e627805a66996839dd0f54ec4670c.data.unityweb",
     "Build/cbe3661c3b0ba3d17490b7f458991a52.wasm.unityweb",
     "TemplateData/style.css"
 
